0383de85fb9939561af465f5b7676dac1f8c325e
subhransu-subudhi-87 <subhransu-subudhi87@gmail.com>
2021-09-29 16:59:06 +0530

    Mocking exception scenarios committed

--- order-processing-service/test/com/mocking/bo/OrderBOImplTest.java
+++ order-processing-service/test/com/mocking/bo/OrderBOImplTest.java
@@ -1,13 +1,16 @@
 package com.mocking.bo;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import org.junit.Before;
+//import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.stubbing.OngoingStubbing;
 
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
 import java.sql.SQLException;
@@ -16,10 +19,11 @@ import com.mocking.bo.exception.BOException;
 import com.mocking.dao.OrderDAO;
 import com.mocking.dto.Order;
 
-class OrderBOImplTest {
+public class OrderBOImplTest {
 
 	@Mock
 	private OrderDAO dao;
+	private OrderBOImpl bo;
 	
 	@Before
 	public void setup() {
@@ -29,13 +33,13 @@ class OrderBOImplTest {
 		 * all the Objects present in the class marked with @Mock Annotation.
 		 */
 		MockitoAnnotations.initMocks(this);
+		bo = new OrderBOImpl();
+		bo.setOrderDao(dao);
 	}
 	
 	@Test
-	void placeorder_should_create_an_order() throws SQLException, BOException {
+	public void placeorder_should_create_an_order() throws SQLException, BOException {
       
-		OrderBOImpl bo = new OrderBOImpl();
-		bo.setOrderDao(dao);
 		
 		//Setting the expectations as below
 		Order order = new Order();
@@ -46,6 +50,19 @@ class OrderBOImplTest {
 		assertTrue(result);
 		verify(dao).createOrder(order);
 	
+	}
+
+	@Test(expected=BOException.class)
+	public void placeorder_should_throw_BOException() throws SQLException, BOException {
+      
+		
+		//Setting the expectations as below
+		Order order = new Order();
+		when(dao.createOrder(order)).thenThrow(SQLException.class);
+		
+		//Verifying the results as OrderBOImpl returns true for a successful order creation
+		boolean result = bo.placeOrder(order);
+			
 	}
 
 }
